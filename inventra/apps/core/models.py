52d3e21bae6fd8171fb40c2c457c2349e35d7738
Abdulxadiy <abdulhadiabdurahimov4@gmail.com>
2026-08-02 18:47:27 +0500

    Initial commit

--- inventra/apps/core/models.py
+++ inventra/apps/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    tenant_name = models.ForeignKey('tenants.Tenant', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True

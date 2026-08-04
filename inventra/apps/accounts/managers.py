d0d88fecca9363f666d275f65891ffb90f327c43
Abdulxadiy <abdulhadiabdurahimov4@gmail.com>
2026-08-05 04:35:39 +0500

    Initial commit

--- inventra/apps/accounts/managers.py
+++ inventra/apps/accounts/managers.py
@@ -5,6 +5,7 @@ class UserManager(BaseUserManager):
     def create_user(self, phone_number, password=None, **extra_fields):
         if not phone_number:
             raise ValueError('The phone number must be set!')
+        phone_number = self.normalize_phone_number(phone_number)
         user = self.model(phone_number=phone_number, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -13,9 +14,14 @@ class UserManager(BaseUserManager):
     def create_superuser(self, phone_number, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', 'platform_admin')
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
         return self.create_user(phone_number, password, **extra_fields)
+
+    @staticmethod
+    def normalize_phone_number(phone_number: str) -> str:
+        return phone_number.strip().replace(" ", "").replace('-', '')

27c0659bd00746e9377268654097560ad9ddac2e
Abdulxadiy <abdulhadiabdurahimov4@gmail.com>
2026-08-05 04:37:44 +0500

    Initial commit

--- inventra/apps/accounts/models/user_model.py
+++ inventra/apps/accounts/models/user_model.py
@@ -27,3 +27,16 @@ class User(AbstractBaseUser, PermissionsMixin):
         max_length=20, choices=ROLE_CHOICES,
         default='customer'
     )
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_phone_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username or self.phone_number

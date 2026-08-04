2189fc5834677a0ee1e492195ce56f4414a32f54
Abdulxadiy <abdulhadiabdurahimov4@gmail.com>
2026-08-05 04:37:14 +0500

    Initial commit

---
+++ inventra/apps/accounts/models/user_model.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    ROLE_CHOICES = [
+        ('customer', "Xaridor"),
+        ('staff', "Sotuvchi"),
+        ('owner', "Do'kon egasi"),
+        ('platform_admin', "Platforma administratori"),
+    ]
+
+    phone_number = models.CharField(max_length=15, unique=True)
+    username = models.CharField(max_length=30, unique=True, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+
+    first_name = models.CharField(max_length=50, null=True, blank=True)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    tenant = models.ForeignKey(
+        'tenants.Tenant', on_delete=models.CASCADE,
+        null=True, blank=True, related_name='user'
+    )
+    role = models.CharField(
+        max_length=20, choices=ROLE_CHOICES,
+        default='customer'
+    )

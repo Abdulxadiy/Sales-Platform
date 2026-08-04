bdd01ea31cad2bdd27733f60a68403f3ac0cb5f3
Abdulxadiy <abdulhadiabdurahimov4@gmail.com>
2026-08-05 04:44:38 +0500

    Initial commit

--- inventra/apps/accounts/models/user_model.py
+++ inventra/apps/accounts/models/user_model.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
-from .managers import UserManager
+from ..managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):

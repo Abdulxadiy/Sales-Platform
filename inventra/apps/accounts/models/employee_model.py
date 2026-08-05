60663f2ebc7c234d4209272e525710fed415269d
Abdulxadiy <abdulhadiabdurahimov4@gmail.com>
2026-08-05 11:52:44 +0500

    Initial commit

--- inventra/apps/accounts/models/employee_model.py
+++ inventra/apps/accounts/models/employee_model.py
@@ -4,6 +4,25 @@ from django.db import models
 
 
 class Employee(models.Model):
-    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='employments')
-    tenant = models.ForeignKey('tenants.Tenant', on_delete=models.CASCADE, related_name='employees')
-    position = models.CharField(max_length=100, null=True)
+    user = models.ForeignKey(
+        'accounts.User', on_delete=models.CASCADE, related_name='employments'
+    )
+    tenant = models.ForeignKey(
+        'tenants.Tenant', on_delete=models.CASCADE, related_name='employees'
+    )
+
+    position = models.CharField(max_length=100, blank=True)
+
+    hired_at = models.DateTimeField(auto_now_add=True)
+    fired_at = models.DateTimeField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    hired_by = models.ForeignKey(
+        'accounts.User', on_delete=models.SET_NULL, null=True,
+        blank=True, related_name='hired_employees'
+    )
+    fired_by = models.ForeignKey(
+        'accounts.User', on_delete=models.SET_NULL, null=True,
+        blank=True, related_name='fired_employees'
+    )
+

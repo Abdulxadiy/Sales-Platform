f8e5c5b236365916f9bb83c7a082ac4f22178707
Abdulxadiy <abdulhadiabdurahimov4@gmail.com>
2026-08-16 15:57:28 +0500

    Initial commit

---
+++ inventra/apps/accounts/services/otp_services.py
@@ -0,0 +1,3 @@
+import secrets
+import redis
+from django.conf import settings
--- inventra/apps/tg_bot/services.py
+++ inventra/apps/tg_bot/services.py
@@ -10,10 +10,18 @@ def send_telegram_message(chat_id: str, text: str) -> bool:
     try:
         response = requests.post(
             TELEGRAM_API_URL,
-            json={"chat_id": chat_id, "text": text},
+            json={
+                "chat_id": chat_id,
+                "text": text
+            },
             timeout=5,
         )
         response.raise_for_status()
+
+        data = response.json()
+        if not data.get("ok"):
+            logger.error(f"Telegram API error: %s", data)
+            return False
         return True
     except Exception as e:
         logger.error(f"Telegram send failed for chat_id: {chat_id} | error: {e}")

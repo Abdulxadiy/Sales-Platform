94f2f52a3e60ffcd6654ef8152f61ae1b1dc4594
Abdulxadiy <abdulhadiabdurahimov4@gmail.com>
2026-08-22 08:55:53 +0500

    set up gunicorn

---
+++ inventra/gunicorn.conf.py
@@ -0,0 +1,29 @@
+
+import multiprocessing
+import os
+
+
+workers = int(os.environ.get("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2 + 1))
+
+worker_class = os.environ.get("GUNICORN_WORKER_CLASS", "gevent")
+
+worker_connections = 1000
+
+timeout = 30
+
+graceful_timeout = 30
+
+max_requests = 1000
+
+max_requests_jitter = 100
+
+accesslog = "-"
+errorlog = "-"
+
+access_log_format = '%(h)s "%(r)s" %(s)s %(D)sus'
+
+loglevel = os.environ.get("GUNICORN_LOG_LEVEL", "info")
+
+preload_app = True
+
+proc_name = "inventra_gunicorn"
